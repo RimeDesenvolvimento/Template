12e6087135c2a5c494dca72ca8ff937f31bbc070
Douglas <douglas@rime.com.br>
2018-10-19 10:44:28 -0300

    Ajuste de URL

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,4 +1,4 @@
-//= lib/responsive-bootstrap-toolkit/bootstrap-toolkit.min.js
-//= lib/clickout/jquery.clickout.js
-//= lib/jScrollPane/jquery.mousewheel.js
-//= lib/jScrollPane/jquery.jscrollpane.min.js
+//= https://raw.githubusercontent.com/RimeDesenvolvimento/Template/master/src/js/lib/responsive-bootstrap-toolkit/bootstrap-toolkit.min.js
+//= https://raw.githubusercontent.com/RimeDesenvolvimento/Template/master/src/js/lib/clickout/jquery.clickout.js
+//= https://raw.githubusercontent.com/RimeDesenvolvimento/Template/master/src/js/lib/jScrollPane/jquery.mousewheel.js
+//= https://raw.githubusercontent.com/RimeDesenvolvimento/Template/master/src/js/lib/jScrollPane/jquery.jscrollpane.min.js
